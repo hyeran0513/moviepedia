a66d240029a2b806e7d288384d29759151c2b42a
hyeran0513 <hr_0513@naver.com>
2025-01-18 23:48:55 +0900

    250118 김혜란 메인 페이지 작업 완료
    - 영화 상세 정보를 고해상도로 출력
    - 영화 포스터가 없을 경우 대체 이미지를 넣음
    - 검색 영역에서 제목과 년도 모두 입력되지 않으면 경고 메시지가 표시되도록 유효성 검사 추가
    - Swiper에 5개의 임시 영화를 삽입
    - 영화 검색 결과에서 imdbID 값을 가져와 해당 영화의 상세 정보를 조회하여 출력
    - 영화 검색 버튼을 클릭할 때, title 또는 year 값을 result.html로 파라미터로 전달

---
+++ assets/js/components/swiper.js
@@ -0,0 +1,63 @@
+import { fetchMovie } from "../api/fetch-movie.js";
+
+export const initializeSwiper = (containerSelector, options = {}) => {
+  return new Swiper(containerSelector, {
+    effect: 'fade',
+    loop: true,
+    autoplay: {
+      delay: 3000,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    ...options,
+  });
+};
+
+export const renderSwiperMovies = async () => {
+  const swiperWrapper = document.querySelector(".swiper-wrapper");
+  const data = await fetchMovie("ironman");
+
+  if (data.movies) {
+    swiperWrapper.innerHTML = data.movies
+      .map((movie) => {
+        return `
+          <div class="swiper-slide">
+            <div class="movie-poster">
+              ${movie.details.Poster === 'N/A' ? `
+                <div class="movie-poster--default">
+                  <i class='bx bx-image-alt'></i>
+                </div>
+              ` : `
+                <img src="${movie.details.Poster}" alt="${movie.Title}" />
+              `}
+            </div>
+
+            <div class="movie-container">
+              <div class="movie-info">
+                <h3 class="movie-title">${movie.Title}</h3>
+                <div class="movie-details">
+                  <p class="movie-runtime">
+                    <i class='bx bx-time' ></i>
+                    <span>${movie.details.Runtime !== "N/A" ? movie.details.Runtime : "0 min"}</span>
+                  </p>
+                  <p class="movie-rating">
+                    <i class='bx bx-star'></i>
+                    <span>${movie.details.imdbRating !== "N/A" ? movie.details.imdbRating : 0} / 10</span>
+                  </p>
+                </div>
+
+                <div class="movie-content">${movie.details.Plot !== "N/A" ? movie.details.Plot : "Description not available"}</div>
+              </div>
+            </div>
+          </div>
+        `;
+      })
+      .join("");
+
+    initializeSwiper(".swiper-container");
+  } else {
+    console.error("movies fetch 에러", data);
+  }
+};
--- assets/js/utils/index.js
+++ assets/js/utils/index.js
@@ -1,5 +1,29 @@
 import { initializeWeb } from './main.js';
-import { displayMovies } from '../components/card.js';
+import { initializeSwiper, renderSwiperMovies } from '../components/swiper.js';
+import { createYearOptions } from '../components/select.js';
+import { submitForm, deleteTitle, toggleDeleteButton  } from '../components/search-form.js';
 
-initializeWeb();
-displayMovies();
+document.addEventListener('DOMContentLoaded', () => {
+  // 웹 초기화 설정
+  initializeWeb();
+
+  // Swiper 초기화
+  initializeSwiper('.swiper-container', {
+    slidesPerView: 1,
+  });
+
+  // 스와이퍼 무비 렌더링
+  renderSwiperMovies();
+
+  // 셀렉트 박스 옵션 설정 (1900년 ~ 현재 연도)
+  createYearOptions('selectYear', 1900);
+
+  // searchForm의 제출 버튼을 눌렀을 때 검색어와 선택된 년도 파라미터로 넘겨줌
+  document.getElementById("searchForm").addEventListener("submit", submitForm);
+
+  // 타이틀 검색 삭제 버튼
+  document.querySelector(".form-button--delete").addEventListener("click", deleteTitle);
+
+  // 검색 삭제 버튼 노출 토글
+  toggleDeleteButton();
+});
