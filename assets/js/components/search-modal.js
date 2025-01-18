50926993ec0275f4d95cb227deeb069e58978e14
sejin <neokin7734@naver.com>
2025-01-18 20:19:44 +0900

    250118 박세진 search-modal 작성. 오류 수정필요

--- assets/js/components/search-modal.js
+++ assets/js/components/search-modal.js
@@ -1,3 +1,5 @@
+import { fetchExample } from "../api/fetch-example.js";
+
 export const loadSearchModal = () => {
   const url =
     window.location.hostname === "hyeran0513.github.io"
@@ -24,4 +26,48 @@ export const loadSearchModal = () => {
       });
     })
     .catch((error) => console.error("모달 fetch 오류:", error));
+
 };
+
+
+document.addEventListener("DOMContentLoaded" , async function(){ 
+  searchModule();
+})
+
+// 외부 모듈상태에서 DOMContentsLoad 옵션이 작동이 제대로 안됌.
+// search-modal.js가 searchModal.html보다 먼저 로딩되고있음
+async function searchModule () {
+    // const searchTarget = document.getElementsByClassName('modal-search__title');
+    const searchTarget = document.querySelector('modal-search__title');
+
+    console.log( 'searchTarget : ', searchTarget);
+
+    searchTarget.addEventListener( 'keyup' , async (e) => {
+      console.log('event : ' , e);
+      console.log( '입력한 제목 : ', searchTarget.innerHTML );
+
+      try{ 
+        if(searchTarget.value === null ){
+          console.log( '입력한 제목이 없음 ');
+          return;
+        }
+        
+        console.log( '입력한 제목 : ', searchTarget.value );
+        const result = await fetchExample(searchTarget.value);
+
+        console.log( '결과 : ', result);
+
+        /* <i class='bx bx-time' ></i> */
+
+      }catch(error){
+        throw new Error('예외 상황 발생 : ' + error );
+      }
+    });
+}
+
+// const searchTest = function(callback){
+//   document.readyState === "interactive" || document.readyState ==="complete" 
+//     ? callback() : document.addEventListener("DOMContentLoaded", callback);
+// }
+
+// searchTest(searchModule);
