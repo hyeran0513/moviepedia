9c675c3dd2c1655d5a9d7410cb8b6b81937bfbc8
sejin <neokin7734@naver.com>
2025-01-20 22:50:57 +0900

    250120 박세진 개발용 로그 제거

--- assets/js/components/search-modal.js
+++ assets/js/components/search-modal.js
@@ -29,7 +29,6 @@ export const loadSearchModal = () => {
       const searchTarget = document.querySelector(".modal-search__title");
       let existSearch = false, existDelete = false;
       if (searchTarget) {
-        // console.log(" 검색 모듈 로직 실행전 서치 모달창 입력창 등록 완료");
         existSearch = true;
         setupSearchHandler(searchTarget);
       }
@@ -37,48 +36,13 @@ export const loadSearchModal = () => {
       // 검색 모듈 로직 실행전 서치 모달창 삭제버튼 등록
       const deleteButton = document.querySelector(".modal-search__delete");
       if (deleteButton) {
-        // console.log("검색 모듈 로직 실행전 서치 모달창 삭제버튼 등록 완료");
         existDelete = true;
         searchTitleInit();
       }
 
-      // 검색 모듈 로직 실행
-      // initializeSearchModule();
     })
     .catch((error) => console.error("모달 fetch 오류:", error));
 };
-
-// DOM 변화를 감지하기 위한 MutationObserver
-// function initializeSearchModule() {
-//   const searchModal = document.getElementById("searchModal");
-
-//   if (!searchModal) {
-//     console.error("searchModal 요소를 찾을 수 없습니다.");
-//     return;
-//   }
-
-//   const observer = new MutationObserver(() => {
-//     const searchTarget = document.querySelector(".modal-search__title");
-//     let existSearch = false, existDelete = false;
-//     if (searchTarget) {
-//       console.log("서치 모달창 입력창 등록 완료");
-//       existSearch = true;
-//       setupSearchHandler(searchTarget);
-//     }
-//     const deleteButton = document.querySelector(".modal-search__delete");
-//     if (deleteButton) {
-//       console.log("서치 모달창 삭제버튼 등록 완료");
-//       existDelete = true;
-//       searchTitleInit();
-//     }
-
-//     if(existSearch && existDelete ){
-//       observer.disconnect();
-//     }
-//   });
-
-//   observer.observe(searchModal, { childList: true, subtree: true });
-// }
 
 // 서치모달 스크롤시 추가 검색용도의 프로퍼티
 let now = 10;
@@ -97,23 +61,16 @@ async function setupSearchHandler(searchTarget) {
   searchTarget.addEventListener("keyup", async (e) => {
     try {
       if (!searchTarget.value) {
-        // console.log("입력한 제목이 없음");
         return;
       }
 
       controller.abort();
-      // console.log("입력한 제목: ", searchTarget.value);
       const result = await movieSearch.getMovieByTitle(searchTarget.value)
       
       console.log("결과: ", result);
 
       let searchResult = await setupMovieContents(result).then();
       console.log("결과 포맷팅 : " , searchResult );
-      // if( 0 < searchResult.indexOf('modal-search__alert') ){
-      //   list.innerHTML = searchResult;
-      // }else{
-      //   list.innerHTML += searchResult;
-      // }
       
       list.innerHTML = searchResult;
     } catch (error) {
@@ -133,12 +90,9 @@ async function setupMovieContents( jsonData ){
 
   try{
     for( const movie of jsonData.movies){
-      // jsonData.movies.forEach( async (movie) => {
         console.log( movie);
       let runTime = await ( movieSearch.getMovieByImdbID(movie.imdbID) );
       
-      // console.log( "런타임정보를 요청하기 위한 상세정보 : ", runTime  );
-      // console.log( "런타임정보 : ", runTime.Runtime  );
       element += `
           <div class="modal-search__ele">
             <div class="modal-search__ele__img">
@@ -167,8 +121,6 @@ async function setupMovieContents( jsonData ){
 
           </div>
       `;
-      // console.log(element);
-    // });
     }
 
     return element;
