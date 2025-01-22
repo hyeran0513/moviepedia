a6c0acebda1c3d8c5db9a236c480e496a3fdff03
phoebe7010 <phoebe7010@daum.net>
2025-01-22 17:39:43 +0900

    250122 최승이 result.html 데이터 불러오기

--- assets/js/components/swiper.js
+++ assets/js/components/swiper.js
@@ -1,37 +1,41 @@
-import { fetchMovie } from "../api/fetch-movie.js";
+import { fetchMovie } from '../api/fetch-movie.js';
 
 export const initializeSwiper = (containerSelector) => {
-  return new Swiper(containerSelector, {
-    effect: 'fade',
-    loop: true,
-    slidesPerView: 1,
-    autoplay: {
-      delay: 3000,
-    },
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
-  });
+	return new Swiper(containerSelector, {
+		effect: 'fade',
+		loop: true,
+		slidesPerView: 1,
+		autoplay: {
+			delay: 3000,
+		},
+		pagination: {
+			el: '.swiper-pagination',
+			clickable: true,
+		},
+	});
 };
 
 export const renderSwiperMovies = async () => {
-  const swiperWrapper = document.querySelector(".swiper-wrapper");
-  const data = await fetchMovie("ironman");
+	const swiperWrapper = document.querySelector('.swiper-wrapper');
+	const data = await fetchMovie('ironman');
 
-  if (data.movies) {
-    swiperWrapper.innerHTML = data.movies
-      .map((movie) => {
-        return `
+	if (data.movies) {
+		swiperWrapper.innerHTML = data.movies
+			.map((movie) => {
+				return `
           <div class="swiper-slide">
             <div class="movie-poster">
-              ${movie.details.Poster === 'N/A' ? `
+              ${
+								movie.details.Poster === 'N/A'
+									? `
                 <div class="movie-poster--default">
                   <i class='bx bx-image-alt'></i>
                 </div>
-              ` : `
+              `
+									: `
                 <img src="${movie.details.Poster}" alt="${movie.Title}" />
-              `}
+              `
+							}
             </div>
 
             <div class="movie-container">
@@ -40,15 +44,15 @@ export const renderSwiperMovies = async () => {
                 <div class="movie-details">
                   <p class="movie-runtime">
                     <i class='bx bx-time' ></i>
-                    <span>${movie.details.Runtime !== "N/A" ? movie.details.Runtime : "0 min"}</span>
+                    <span>${movie.details.Runtime !== 'N/A' ? movie.details.Runtime : '0 min'}</span>
                   </p>
                   <p class="movie-rating">
                     <i class='bx bx-star'></i>
-                    <span>${movie.details.imdbRating !== "N/A" ? movie.details.imdbRating : 0} / 10</span>
+                    <span>${movie.details.imdbRating !== 'N/A' ? movie.details.imdbRating : 0} / 10</span>
                   </p>
                 </div>
 
-                <div class="movie-content">${movie.details.Plot !== "N/A" ? movie.details.Plot : "Description not available"}</div>
+                <div class="movie-content">${movie.details.Plot !== 'N/A' ? movie.details.Plot : 'Description not available'}</div>
               
                 <div class="btn-wrap">
                   <a href="/detail.html?imdbID=${movie.imdbID}" class="btn-primary movie-btn-detail">
@@ -60,11 +64,11 @@ export const renderSwiperMovies = async () => {
             </div>
           </div>
         `;
-      })
-      .join("");
+			})
+			.join('');
 
-    initializeSwiper(".swiper-container");
-  } else {
-    console.error("movies fetch 에러", data);
-  }
+		initializeSwiper('.swiper-container');
+	} else {
+		console.error('movies fetch 에러', data);
+	}
 };
