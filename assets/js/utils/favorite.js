68924f8b54062c02132c200e03b6cc1d84877e7f
hyeran0513 <hr_0513@naver.com>
2025-01-23 16:23:51 +0900

    250123 김혜란 type="modal" 설정 시 DOMContentLoaded 이벤트 자동 적용, 별도 처리 불필요

--- assets/js/utils/favorite.js
+++ assets/js/utils/favorite.js
@@ -6,21 +6,19 @@ import {
   toggleDeleteButton,
 } from "../components/filter/filter.js";
 
-document.addEventListener("DOMContentLoaded", () => {
-  initializeWeb();
+initializeWeb();
 
-  const favorites = JSON.parse(sessionStorage.getItem("favorites")) || [];
+const favorites = JSON.parse(sessionStorage.getItem("favorites")) || [];
 
-  // searchForm의 제출 버튼 클릭 시 이벤트
-  document.getElementById("searchForm").addEventListener("click", (e) => {
-    submitForm(favorites, e);
-  });
-
-  createCard(favorites, "favorite");
-
-  // 타이틀 검색 삭제 버튼
-  document.querySelector(".btn-del").addEventListener("click", deleteTitle);
-
-  // 검색 삭제 버튼 노출 토글
-  toggleDeleteButton();
+// searchForm의 제출 버튼 클릭 시 이벤트
+document.getElementById("searchForm").addEventListener("click", (e) => {
+  submitForm(favorites, e);
 });
+
+createCard(favorites, "favorite");
+
+// 타이틀 검색 삭제 버튼
+document.querySelector(".btn-del").addEventListener("click", deleteTitle);
+
+// 검색 삭제 버튼 노출 토글
+toggleDeleteButton();
--- assets/js/utils/result.js
+++ assets/js/utils/result.js
@@ -2,17 +2,15 @@ import { initializeWeb } from "./main.js";
 import { createCard } from "../components/card/card.js";
 import { getMovies } from "../api/movie.js";
 
-document.addEventListener("DOMContentLoaded", async () => {
-  initializeWeb();
+initializeWeb();
 
-  const url = new URL(window.location.href);
-  const movieTitle = url.searchParams.get("title");
-  const movieYear = url.searchParams.get("year");
+const url = new URL(window.location.href);
+const movieTitle = url.searchParams.get("title");
+const movieYear = url.searchParams.get("year");
 
-  try {
-    const resultMovies = await getMovies(movieTitle, movieYear);
-    createCard(resultMovies, "result");
-  } catch (error) {
-    console.error("Error fetching movies:", error);
-  }
-});
+try {
+  const resultMovies = await getMovies(movieTitle, movieYear);
+  createCard(resultMovies, "result");
+} catch (error) {
+  console.error("Error fetching movies:", error);
+}
